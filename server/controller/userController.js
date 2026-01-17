83bcd0b60dde6c44db6fd41e2c2b8e80b94c6d1a
Md Laju Mia <developerlaju@gmail.com>
2026-01-17 09:03:32 +0600

    Post Method is Implemented

---
+++ server/controller/userController.js
@@ -0,0 +1,20 @@
+import User from "../model/userModel.js";
+
+export const create = async(req,res) =>{
+    try{
+        const newUser = new User(req.body);
+        const {email} = newUser;
+
+        const userExist = await User.findOne({email});
+
+        if(userExist){
+            return res.status(400).json({message : "User Already exist"});
+        }
+
+        const saveData = await newUser.save();
+        res.status(200).json(saveData);
+
+    }catch(error){
+        res.status(500).json({errorMessage:error.message});
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import route from "./routes/userRoutes.js"
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000 ;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+        .connect(MONGOURL)
+        .then(()=>{
+            console.log("BD connected successfully.");
+            app.listen(PORT,()=>{
+                console.log(`Server is running on port : localhost:${PORT}`)
+            });
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+
+app.use("/api", route);
